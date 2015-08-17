b28edb7670c414dc0e90be456f728df8ba96af0b
Eyal Levin <eyalev@gmail.com>
2015-08-17 13:33:48 +0300

    Add packages_directory method.

--- utils2/util.py
+++ utils2/util.py
@@ -1,4 +1,5 @@
 import sys
+import distutils.sysconfig
 
 
 class Util(object):
@@ -8,3 +9,18 @@ class Util(object):
 
     def print_paths(cls):
         print('\n'.join(sys.path))
+
+    def packages_directory(self):
+        print(distutils.sysconfig.get_python_lib())
+
+    def cli_prompt(self):
+
+        return __name__ == '__main__'
+
+    def hello(self):
+
+        print('Hello Util.')
+
+if Util().cli_prompt():
+
+    Util().hello()
