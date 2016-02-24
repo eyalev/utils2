c1b253aa3fb65eba1f74a2235e58b04ef7f708aa
Eyal Levin <eyalev@gmail.com>
2016-02-24 14:24:02 +0200

    Update string to datetime to be more performant.

--- utils2/string_base_util.py
+++ utils2/string_base_util.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from dateutil import parser
 
 
@@ -7,5 +9,26 @@ class StringBaseUtil(object):
         self.string = string
 
     def to_datetime(self):
-        return parser.parse(self.string).replace(tzinfo=None)
 
+        _datetime = None
+
+        formats = [
+            '%Y-%m-%dT%H:%M:%S',
+            '%Y-%m-%dT%H:%M:%SZ',
+            '%Y-%m-%dT%H:%M:%S.%f',
+        ]
+
+        exceptions = []
+
+        for _format in formats:
+
+            try:
+                _datetime = datetime.strptime(self.string, _format)
+                return _datetime
+
+            except ValueError as e:
+                exceptions.append(e)
+
+        if _datetime is None:
+            parsed_datetime = parser.parse(self.string).replace(tzinfo=None)
+            return parsed_datetime
